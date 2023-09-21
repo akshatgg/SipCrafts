e3dde9cd270cca3265f04b04ed53fe3ab100a07c
akshatgg <akshatg9636@gmail.com>
2023-09-22 02:11:05 +0545

    HomePage

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,15 +1,47 @@
 
 function showPopup() {
     const loginPopup = document.getElementById('loginPopup');
-        loginPopup.style.display = 'block';
-    }
+    loginPopup.style.display = 'block';
+}
 
 
 function closePopup() {
     const loginPopup = document.getElementById('loginPopup');
-        loginPopup.style.display = 'none';
-    }
-
+    loginPopup.style.display = 'none';
+}
 
 window.onload = showPopup;
+
+let signinbtn=document.getElementById("signinbtn")
+let signupbtn=document.getElementById("signupbtn")
+let title=document.getElementById("title")
+let firstN=document.getElementById("firstN")
+let lastN=document.getElementById("lastN")
+
+
+let login=true;
+
+// function changelogin(){
+// login=!login;
+// }
+
+function setup(){
+    signinbtn.addEventListener("click",()=>{login=true})
+    signupbtn.addEventListener("click",()=>{login=false})
+    console.log(login);
+    if(login==true){
+
+title.innerText="Sign in";
+firstN.style.display='none';
+lastN.style.display='none';
+}
+else
+{
+    firstN.style.display='block';
+lastN.style.display='block';
+    title.innerText="Sign Up"
+}
+// changelogin();
+}
+
 
