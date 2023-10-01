a084b86a6603a6ee7070134678435e30b21ae320
codecShivam <codecshivam@gmail.com>
2023-10-02 01:13:37 +0530

    fixed blunder

---
+++ HomePage/script.js
@@ -0,0 +1,47 @@
+function showPopup() {
+    const loginPopup = document.getElementById('loginPopup');
+    loginPopup.style.display = 'block';
+}
+
+function closePopup() {
+    const loginPopup = document.getElementById('loginPopup');
+    loginPopup.style.display = 'none';
+}
+
+window.onload = showPopup;   
+
+var signinbtn = document.getElementById("signinbtn");
+var signupbtn = document.getElementById("signupbtn");
+var title = document.getElementById("title");
+var firstN = document.getElementById("firstN");
+var lastN = document.getElementById("lastN");
+var loginbutton=document.getElementById("Login");
+
+
+let login = true;
+
+function setup() {
+    
+    signupbtn.addEventListener("click", () => {
+        login = false;
+        title.innerText = "Sign up";
+        firstN.style.display = 'block';
+        lastN.style.display = 'block';
+        loginbutton.innerText = "Register";
+        return false; 
+    });
+    
+    signinbtn.addEventListener("click", () => {
+        login = true;
+        title.innerText = "Sign in";
+        firstN.style.display = 'none';
+        lastN.style.display = 'none';
+        loginbutton.innerText = "Login";
+        return false; 
+    });
+    
+}
+
+
+// Call the setup function to set up the event listeners
+setup();
