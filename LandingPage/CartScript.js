d71bae0a83a12c3f1feb42a3f4ca78fd2dea03e9
akshatgg <akshatg9636@gmail.com>
2023-10-22 12:40:51 +0545

    add a total amount yu want to be paid

--- LandingPage/CartScript.js
+++ LandingPage/CartScript.js
@@ -1,22 +1,21 @@
-let ShopingCart= document.getElementById('shoping-cart');
-let label=document.getElementById('label');
-let basket=JSON.parse(localStorage.getItem('data')) || []
+let ShopingCart = document.getElementById("shoping-cart");
+let label = document.getElementById("label");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-
-let calculate=()=>{
-let CartIcon=document.getElementById('cart_count')
-CartIcon.innerHTML=basket.length
-}
+let calculate = () => {
+  let CartIcon = document.getElementById("cart_count");
+  CartIcon.innerHTML = basket.length;
+};
 
 //Basket Items//
 
-let generateCart=()=>{
-    if(basket.length != 0){
-        return (ShopingCart.innerHTML=basket.map((x)=>{
-     let { id, name , price ,img ,content}=x;
-    
-     return `
-     <div class="cart_item">
+let generateCart = () => {
+  if (basket.length != 0) {
+    return (ShopingCart.innerHTML = basket.map((x) => {
+      let { id, name, price, img, content } = x;
+
+      return `
+     <div class="cart_item" id='${id}'>
      <p>${name}</p>
      <div class='cart-items'>
      <img src='${img}' alt='' />
@@ -25,22 +24,39 @@ let generateCart=()=>{
      <button class="rmv_btn" onclick="remove_from_Cart(${id})">Remove</button>
      </div>
      
-     `
-        })
-        )
-    }
-}
-generateCart()
-calculate()
+     `;
+    }));
+  }
+};
+generateCart();
+calculate();
 
 //remove from cart//
-let remove_from_Cart=(id)=>{
-    if(basket.length==1){
-    location.reload()
-    }
-    basket=basket.filter((x)=>x.id !=id)
-    localStorage.setItem('data',JSON.stringify(basket))
-    calculate()
-    generateCart()
-}   
+let remove_from_Cart = (id) => {
+  if (basket.length == 1) {
+    location.reload();
+  }
+  basket = basket.filter((x) => x.id != id);
+  localStorage.setItem("data", JSON.stringify(basket));
+  
+  calculate();
+  generateCart();
+  TotalAmount();
+};
+
+//adding total amount you want to paid//
+
+// Define TotalAmount function
+let TotalAmount = () => {
+    let total = 0; // Initialize total to 0
+    basket.forEach((x) => {
+      total += parseInt(x.price, 10); // Parse price as an integer and add to the total
+    });
+  
+    let totalElement = document.querySelector(".total"); // Use a class selector
+    totalElement.innerHTML = total; // Display the total price
+  };
+  
+  // Call TotalAmount function
+  TotalAmount();
 
