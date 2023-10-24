7769e8a3761359a11db1953e2affe995c430051c
akshatgg <akshatg9636@gmail.com>
2023-10-25 01:19:03 +0545

    conflicts resolved

--- LandingPage/script.js
+++ LandingPage/script.js
@@ -1,214 +1,238 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let currentPage = 1;
-  const itemsPerPage = 6;
-  const totalItems = shopItems.length;
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-  const listContainer = document.getElementById('shop');
-  const prevButton = document.querySelector('.Previ');
-  const nextButton = document.querySelector('.Nexts');
-  const pageButtons = document.querySelectorAll('.listpage li');
+const itemsPerPage = 6; // Number of items to display per page
+let currentPage = 1;
 
-  function loadItems() {
-    listContainer.innerHTML = ''; // Clear the list container
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
+function displayProducts(pageNumber) {
+  const shopList = document.getElementById("shop");
+  shopList.innerHTML = ""; // Clear the product list
 
-    for (let i = startIndex; i < endIndex; i++) {
-      const item = shopItems[i];
-      const itemElement = createItemElement(item);
-      listContainer.appendChild(itemElement);
-    }
+  const startIndex = (pageNumber - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
-    updatePagination();
-  }
+  for (let i = startIndex; i < endIndex && i < shopItems.length; i++) {
+    const product = shopItems[i];
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("item");
 
-  function createItemElement(item) {
-    // Create and return an HTML element for the shop item
-    const itemElement = document.createElement('div');
-    itemElement.classList.add('item');
-    itemElement.innerHTML = `
-      <div class="imag">
-        <img src="${item.img}" alt="${item.name}" />
+    // Set the product.id as the id attribute
+    productDiv.id = product.id;
+
+    // Create the product content
+    const productContent = `
+    <div>
+      <div class="imag" id="image">
+        <img src="${product.img}" alt="${product.name}">
       </div>
       <div class="content">
-        <div class="title">${item.name}</div>
-        <div class="details">${item.content}</div>
-        <div class="price">${item.price}</div>
-        <button class="add" onclick="addCart(${item.id}, '${item.name}', ${item.price}, '${item.img}')">Add to cart</button>
+        <div class="title">${product.name}</div>
+        <div class="details">${product.content}</div>
+        <div class="price">Price: $${product.price}</div>
+        <button class="add" onclick="addCart('${product.id}','${product.img}','${product.name}','${product.price}')">Add to Cart</button>
       </div>
+    </div>  
     `;
-    return itemElement;
+
+    productDiv.innerHTML = productContent;
+    shopList.appendChild(productDiv);
+    handleImageHover(productDiv);
   }
+}
 
-  function updatePagination() {
-    pageButtons.forEach((button) => {
-      const pageNumber = parseInt(button.textContent);
-      button.classList.remove('active');
-      if (pageNumber === currentPage) {
-        button.classList.add('active');
-      }
-    });
+// Initial display
+displayProducts(currentPage);
 
-    prevButton.disabled = currentPage === 1;
-    nextButton.disabled = currentPage === totalPages;
+document.getElementById("prevPage").addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    displayProducts(currentPage);
   }
-
-  function changePage(page) {
-    if (page >= 1 && page <= totalPages) {
-      currentPage = page;
-      loadItems();
-    }
-  }
-
-  // Initial page load
-  loadItems();
-
-  // Previous button click event
-  prevButton.addEventListener('click', () => {
-    changePage(currentPage - 1);
-  });
-
-  // Next button click event
-  nextButton.addEventListener('click', () => {
-    changePage(currentPage + 1);
-  });
-
-  // Page button click events
-  pageButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const pageNumber = parseInt(button.textContent);
-      changePage(pageNumber);
-    });
-  });
 });
 
+document.getElementById("nextPage").addEventListener("click", () => {
+  const totalPages = Math.ceil(shopItems.length / itemsPerPage);
+  if (currentPage < totalPages) {
+    currentPage++;
+    displayProducts(currentPage);
+  }
+});
+
+for (let i = 1; i <= 3; i++) {
+  document.getElementById(`page${i}`).addEventListener("click", () => {
+    currentPage = i;
+    displayProducts(currentPage);
+  });
+}
 
 
-let product=[...document.querySelectorAll('.scrolling')]
-let aftbtn=[...document.querySelectorAll('.aftbtn')]
-let prebtn=[...document.querySelectorAll('.prebtn')]
 
 
-product.forEach((item,i)=>
-{
-  let containerdimension=item.getBoundingClientRect();
-  let containerwidth=containerdimension.width;
 
 
-  aftbtn[i].addEventListener('click',()=>{
-      item.scrollLeft += containerwidth;
-      
-  })
-  prebtn[i].addEventListener('click',()=>{
-      item.scrollLeft -= containerwidth;
-  })
-  
+
+
+//scro;;ing///
+let product = [...document.querySelectorAll(".scrolling")];
+let aftbtn = [...document.querySelectorAll(".aftbtn")];
+let prebtn = [...document.querySelectorAll(".prebtn")];
+
+product.forEach((item, i) => {
+  let containerdimension = item.getBoundingClientRect();
+  let containerwidth = containerdimension.width;
+
+  aftbtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerwidth;
+  });
+  prebtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerwidth;
+  });
+
   function scrollToNextImage() {
-      item.scrollLeft += containerwidth;
+    item.scrollLeft += containerwidth;
   }
 
   // Function to scroll to the next image every 5 seconds
   function autoSlide() {
-      setInterval(scrollToNextImage, 7000); // 5000 milliseconds (5 seconds)
+    setInterval(scrollToNextImage, 7000); // 5000 milliseconds (5 seconds)
   }
 
   autoSlide();
-  
-})
+});
+
+
+
+
+
+
+//making local storage//
 
 
 
 // local storage of cart
-
-const shop=document.getElementById("shop")
-
-let basket = JSON.parse(localStorage.getItem('data')) || [];
-let generateshop=()=>{
+// Wrap your code in a DOMContentLoaded event listener
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+let shop = document.getElementById("shop");
+document.addEventListener("DOMContentLoaded", function() {
   
-  shop.innerHTML=shopItems.map((x)=>{
-      let {id,name,price,content,img}=x
+  // Assuming you have selected the "shop" element correctly
+
+  // let generateshop = () => {
+  //   shop.innerHTML = shopItems.map((x) => {
+  //     let { id, name, price, content, img } = x;
+
+  //     return `
+  //       <div class="item" id="${id}">
+  //         <div class="imag">
+  //           <img src="${img}" alt="" />
+  //         </div>
+  //         <div class="content">
+  //           <div class="title">${name}</div>
+  //           <div class="details">${content}</div>
+  //           <div class="price">${price}</div>
+  //        <button class="add" id="add" onclick="addCart('${id}','${name}','${price}','${img}')">Add to cart</button>
+  //         </div>
+  //       </div>`;
+  //   });
+  // };
+
+  // // Call generateshop to populate the "shop" element
+  // generateshop();
+} );
 
 
-      return `
-      <div class="item">
-      <div class="imag">
-          <img src="${img}" alt="" />
-      </div>
-      <div class="content">   
-          <div class="title">${name}</div>
-          <div class="details">${content}
-              </div>
-               <div class="price">${price}</div>
-               <button class="add" id="add" onclick="addCart('${id}','${name}','${price}','${img}')">Add to cart</button>
-      </div>
-  </div> 
-      `
-     
-      
-  
-  })
+let addCart = (id, img, name, price) => {
+  basket.push({
+    id: id,
+    item: 1,
+    name: name,
+    price: price,
+    img: img,
+  });
+  localStorage.setItem("data", JSON.stringify(basket));
+  calculate();
+  // generateshop();
+};
 
-}                                                     
 
-let addCart=(id,name,price,img)=>{
-basket.push({
-id:id,
-item:1,
-name:name,
-price:price,
-img:img
-})
-localStorage.setItem('data',JSON.stringify(basket))
-calculate()
-}
+
 
 // number of cart items //
 
-let calculate=()=>{
-let cart_count=document.getElementById("cart_count")
-// let BtnAddToCart=document.getElementById("add")
-// let count=0;
-// BtnAddToCart.addEventListener('onclick',()=>{
-//   count=count+1
-//   cart_count.innerHTML=count;
-//   })
-  let cart_amount=basket.length
-  cart_count.innerHTML=cart_amount
-}
-
-calculate()
-generateshop();
 
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-let description = document.getElementById("description");
+let calculate = () => {
+  let cart_count = document.getElementById("cart_count");
+  // let BtnAddToCart=document.getElementById("add")
+  // let count=0;
+  // BtnAddToCart.addEventListener('onclick',()=>{
+  //   count=count+1
+  //   cart_count.innerHTML=count;
+  //   })
+  let cart_amount = basket.length;
+  cart_count.innerHTML = cart_amount;
+};
 
-let shopItems = []; // Initialize an empty array to hold the data
+calculate();
+// generateshop();
 
-let hoverjuice = () => {
-  description.addEventListener('mouseover', () => {
-    // Use fetch to load the shopItems from an external JSON file
-    fetch('shopItems.json')
-      .then(response => response.json())
-      .then(data => {
-        shopItems = data; // Assign the loaded data to the shopItems array
-        description.innerHTML = shopItems.map((y) => {
-          let { id, name, price, content, img } = y;
-          return `
-            <div class="hover">
-              <img src="${img}" alt="${name}" />
-              <div class="con">
-                ${content}
-              </div>
+
+
+// Wrap your code in a DOMContentLoaded event listener
+// document.addEventListener("DOMContentLoaded", function () { 
+  // Your existing code
+  
+  // Function to handle hovering over a product image
+  function handleImageHover(product) {
+    let description = document.getElementById("descriptions");
+    const productImage = product.querySelector("img");
+  
+    productImage.addEventListener("mouseenter", () => {
+      const productId = product.getAttribute("id");
+      console.log(productId);
+  
+      // Find the matching product in the shopItems array
+      let productData = null;
+      for (let i = 0; i < shopItems.length; i++) {
+        if (shopItems[i].id === parseInt(productId)) {
+          productData = shopItems[i];
+          break;
+        }
+      }
+  
+      if (productData) {
+        description.innerHTML = `
+          <div class="contain">
+            <div class="img">
+              <img src="${productData.img}" alt="">
             </div>
-          `;
-        }).join('');
-      })
-      .catch(error => console.error('Error loading shopItems:', error));
+            <div class='cont'>
+            "${productData.making}"
+            </div>
+          </div>
+        `;
+      }
+    });
+  
+    productImage.addEventListener("mouseleave", () => {
+      description.innerHTML = ''; // Clear the description when the mouse leaves
+    });
+  }
+  
+  // Find all product items and attach hover behavior
+  const productItems = document.querySelectorAll(".item");
+  productItems.forEach((product) => {
+    handleImageHover(product);
   });
+  
+
+
+
+
+//adding + and - symbol to increase your quantity //
+let quantity=()=>{
+  let cart=document.querySelector(".add")
+  cart.addEventListener('click',()=>{
+    
+  })
 }
 
-hoverjuice();
-});
