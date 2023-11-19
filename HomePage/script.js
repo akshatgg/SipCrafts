7e0136303615436bb0be9223949bcdd08b8c4d81
akshatgg <akshatg9636@gmail.com>
2023-11-19 23:34:27 +0545

    login page setup

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,52 +1,14 @@
-function showPopup() {
-    const loginPopup = document.getElementById('loginPopup');
-    loginPopup.style.display = 'block';
+var popup = document.querySelector("#loginpopup");
+
+function showpopup() {
+    popup.style.display = 'block';
 }
 
-function closePopup() {
-    const loginPopup = document.getElementById('loginPopup');
-    loginPopup.style.display = 'none';
+function closepopup() {
+    popup.style.display = 'none';
 }
 
-window.onload = showPopup;   
-
-var signinbtn = document.getElementById("signinbtn");
-var signupbtn = document.getElementById("signupbtn");
-var title = document.getElementById("title");
-var firstN = document.getElementById("firstN");
-var lastN = document.getElementById("lastN");
-var loginbutton=document.getElementById("Login");
-var emailsignup=document.getElementById("Email");
-var passignup=document.getElementById("password");
-var emailsignin=document.getElementById("emails");
-var passsignin=document.getElementById("passwords");
-      
-
-    let login=true;
-    document.getElementById("signincont").style.display="none";
-        document.getElementById("Logins").style.display="none";
-    signupbtn.addEventListener("click", () => {
-        login=true;
-        title.innerText = "Sign up";
-        document.getElementById("signupcont").style.display="block";
-        document.getElementById("signincont").style.display="none";
-        document.getElementById("Logins").style.display="none";
-        document.getElementById("Login").style.display="block";
-        loginbutton.innerText = "Register";
-        
-    });
-    
-    signinbtn.addEventListener("click", () => {
-        login=false;
-        title.innerText = "Sign in";
-        document.getElementById("signupcont").style.display="none";
-        document.getElementById("signincont").style.display="block  ";
-        loginbutton.style.display="none";   
-        document.getElementById("Logins").style.display="block";
-
-    });
-  
+// popup.style.display = 'none';
 
 
-
-
+window.addEventListener('load', showpopup);
