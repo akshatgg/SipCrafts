81c10a3b0bcc48fc24762ee8cf769f87e8ad0683
akshatgg <akshatg9636@gmail.com>
2023-11-25 00:54:51 +0545

    makeing env

--- HomePage/app.js
+++ HomePage/app.js
@@ -1,39 +1,40 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import {getDatabase,set,ref } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+// import {getDatabase,set,ref } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBKkO-p8tKkXXDNLFLUP7f2fKX4JdDRENE",
-  authDomain: "verify-password-70ada.firebaseapp.com",
-  projectId: "verify-password-70ada",
-  storageBucket: "verify-password-70ada.appspot.com",
-  messagingSenderId: "1079707843778",
-  appId: "1:1079707843778:web:22dc2b96b2e30b37c24f5b",
-  measurementId: "G-29SJL1LY0X"
-};    
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database=getDatabase(app);
-const auth = getAuth();
-// const analytics = getAnalytics(app);
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "verify-password-70ada.firebaseapp.com",
+//   projectId: "verify-password-70ada",
+//   storageBucket: "verify-password-70ada.appspot.com",
+//   messagingSenderId: "1079707843778",
+//   appId: "1:1079707843778:web:22dc2b96b2e30b37c24f5b",
+//   measurementId: "G-29SJL1LY0X"
+// };    
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+    
+// const database=getDatabase(app);
+// const auth = getAuth();
+// // const analytics = getAnalytics(app);
 
 
-var signinbtn = document.getElementById("signinbtn");
-var signupbtn = document.getElementById("signupbtn");
-var title = document.getElementById("title");
-var firstN = document.getElementById("firstN");
-var lastN = document.getElementById("lastN");
-var loginbutton=document.getElementById("Login");
-const passwords = document.getElementById("password").value;
-const email = document.getElementById("Email").value;
+// var signinbtn = document.getElementById("signinbtn");
+// var signupbtn = document.getElementById("signupbtn");
+// var title = document.getElementById("title");
+// var firstN = document.getElementById("firstN");
+// var lastN = document.getElementById("lastN");
+// var loginbutton=document.getElementById("Login");
+// const passwords = document.getElementById("password").value;
+// const email = document.getElementById("Email").value;
 
 
 
@@ -44,52 +45,80 @@ const email = document.getElementById("Email").value;
 
 
  
-// document.getElementById("Login").addEventListener("click", function () {
-//     signInWithEmailAndPassword(auth, email, passwords)
-//     .then((userCredential) => {
-//           console.log("RANDI");
-//         const user = userCredential.user;
+// // document.getElementById("Login").addEventListener("click", function () {
+// //     signInWithEmailAndPassword(auth, email, passwords)
+// //     .then((userCredential) => {
+// //           console.log("RANDI");
+// //         const user = userCredential.user;
         
-//           document.getElementById("logi").style.display = "none";
-//           document.getElementById("loginPopup").style.display = "none";
+// //           document.getElementById("logi").style.display = "none";
+// //           document.getElementById("loginPopup").style.display = "none";
         
-//       })
-//       .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         console.error(errorCode, errorMessage); 
-//         window.onload;
-//       });
-//   });
+// //       })
+// //       .catch((error) => {
+// //         const errorCode = error.code;
+// //         const errorMessage = error.message;
+// //         console.error(errorCode, errorMessage); 
+// //         window.onload;
+// //       });
+// //   });
 
 
 
 
-loginbutton.addEventListener('click',(e)=>{
-  var firstN = document.getElementById("firstN");
-  var lastN = document.getElementById("lastN");
+// loginbutton.addEventListener('click',(e)=>{
+//   var firstN = document.getElementById("firstN");
+//   var lastN = document.getElementById("lastN");
 
-  const passwords = document.getElementById("password").value;
-  const email = document.getElementById("Email").value;
+//   const passwords = document.getElementById("password").value;
+//   const email = document.getElementById("Email").value;
   
 
-  createUserWithEmailAndPassword(auth, email, passwords)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    set(ref(database, 'users/'  +  user.uid),{
-      email: email,
-      firstN: firstN,
-      lastN: lastN
+//   createUserWithEmailAndPassword(auth, email, passwords)
+//   .then((userCredential) => {
+//     // Signed up 
+//     const user = userCredential.user;
+//     set(ref(database, 'users/'  +  user.uid),{
+//       email: email,
+//       firstN: firstN,
+//       lastN: lastN
       
-    })
-    alert('user created');
-    // ...
+//     })
+//     alert('user created');
+//     // ...
 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert(errorMessage);
-})
-})
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     alert(errorMessage);
+// })
+// })
+
+
+
+
+
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "",
+    authDomain: "auth-4f3a1.firebaseapp.com",
+    projectId: "auth-4f3a1",
+    storageBucket: "auth-4f3a1.appspot.com",
+    messagingSenderId: "718871430182",
+    appId: "1:718871430182:web:8166623958be0fa78f12e7",
+    measurementId: "G-Q1LLP7LFS2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
