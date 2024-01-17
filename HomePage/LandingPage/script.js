94bfb158086c1cb4b3417183efbbb6df7115b22f
akshatgg <akshatg9636@gmail.com>
2024-01-17 23:26:12 +0545

    adding sign out function

--- HomePage/LandingPage/script.js
+++ HomePage/LandingPage/script.js
@@ -213,3 +213,44 @@ let quantity = () => {
   })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var popup = document.querySelector("#loginpopup");
+
+function showpopup() {
+    popup.style.display = 'block';
+}
+
+function closepopup() {
+    popup.style.display = 'none';
+}
+
+// popup.style.display = 'none';
+
+
+window.addEventListener('load', showpopup);
+
+
+
+
+
+
+
+
+
+
